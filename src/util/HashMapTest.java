29fc94ff889114dbc12f4bd4d4ae91c0a7e231fc
wonhoLee <lwonho@hotmail.com>
2018-08-27 15:31:30 +0900

    test

---
+++ src/util/HashMapTest.java
@@ -0,0 +1,13 @@
+package util;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+
+	public static void main(String[] args) {
+		HashMap<Integer, Integer> hTemp = new HashMap<>();
+		
+		System.out.println(hTemp.containsKey(3));
+	}
+
+}
--- src/util/LinkedListTest.java
+++ src/util/LinkedListTest.java
@@ -7,20 +7,51 @@ public class LinkedListTest {
 
 	public static void main(String[] args) {
 		List<Integer> lTemp = new LinkedList<>();
+		
+		System.out.println(lTemp.size());
+		
 		lTemp.add(1);
 		lTemp.add(2);
 		lTemp.add(1);
 		lTemp.add(2);
 		
-		System.out.println(lTemp.indexOf(1));
+		/*System.out.println(lTemp.indexOf(1));
 		System.out.println(lTemp.indexOf(2));
 		
 		System.out.println(lTemp.get(0));
 		System.out.println(lTemp.get(1));
 		
-		lTemp.remove(1);
+		lTemp.remove(1);*/
+		
+		/*System.out.println(lTemp.toString());
+		
+		lTemp.sort(null);*/
 		
 		System.out.println(lTemp.toString());
+		lTemp.add(0, 9);
+		
+		System.out.println(lTemp.toString());
+		
+		/*int nBeforeValue = lTemp.get(0);
+		int nCnt = 1;
+		int nValue = 2;
+		boolean bTrue = false;
+		for (int i = 1; i < lTemp.size(); i++) {
+			if(nBeforeValue == lTemp.get(i)) {
+				nCnt++;
+			}else {
+				if(nCnt == nValue) {
+					bTrue = true;
+					break;
+				}else {
+					nCnt=1;
+					nBeforeValue = lTemp.get(i);
+				}
+			}
+		}
+		
+		System.out.println(bTrue);
+		*/
 	}
 
 }
